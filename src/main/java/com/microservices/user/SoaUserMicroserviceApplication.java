31d24d0576c140ea7f5d65443fd9822f267a8cea
teuzed <georguiazelpachecos@gmail.com>
2024-09-06 15:20:36 -0500

    [feat]: first commit

---
+++ src/main/java/com/microservices/user/SoaUserMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoaUserMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaUserMicroserviceApplication.class, args);
+    }
+
+}
